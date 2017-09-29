e36f7b3de4e55c74c9f2bdbf4de51c01905ff706
Coulby Nguyen <nguyenco@flip2.engr.oregonstate.edu>
2017-09-29 16:25:54 -0700

    gave you a lil insight of what i want

---
+++ gpacalculator.cpp
@@ -0,0 +1,4 @@
+/*What i want out of this program is that the enter a number of credits for a course and the grade they got. And then you convert that grade from
+ * characters like A- to 3.75 yadda yadda. The easiest way this can be done is through maybe a switch case or else ifs but whatever
+ * Also if you could try to make the function use pass by reference or pointers that'd be great If you need any help ill explain since you are
+ * fairly new to this language -LUV TUM*/
